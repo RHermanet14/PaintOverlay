4954ff1e497e9c6e88b6e8d1742ba9e380353004
RHermanet14 <rhermanet14@gmail.com>
2025-12-30 13:24:16 -0500

    can draw now

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,7 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Ink;
 
 namespace PaintOverlay
 {
@@ -18,19 +18,56 @@ namespace PaintOverlay
     public partial class MainWindow : Window
     {
         private readonly KeyboardHook hook;
+        private readonly DrawingAttributes PenAttributes = new()
+        {
+            Color = Colors.Black,
+            Height = 2,
+            Width = 2,
+        };
+        private readonly DrawingAttributes HighlighterAttributes = new()
+        {
+            Color = Colors.Yellow,
+            Height = 10,
+            Width = 2,
+            IgnorePressure = true,
+            IsHighlighter = true,
+            StylusTip = StylusTip.Rectangle,
+        };
+
+
         public MainWindow()
         {
             InitializeComponent();
+            DrawingCanvas.DefaultDrawingAttributes = PenAttributes;
             hook = new KeyboardHook();
             KeyboardHook.KeyboardInput += OnKeyboardInput;
         }
         private void OnKeyboardInput(object? sender, EventArgs e)
         {
-            if (Keyboard.IsKeyDown(Key.Oem3))
+            if (Keyboard.IsKeyDown(Key.RightAlt))
             {
                 DrawingCanvas.Visibility = DrawingCanvas.Visibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
                 this.Activate();
             }
+            if (Keyboard.IsKeyDown(Key.RightCtrl) && PaintWindow.IsActive)
+            {
+                DrawingMenu.Visibility = DrawingMenu.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingCanvas.Strokes.Clear();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Will copy picture of desktop with ink canvas without top bar into clipboard");
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Will open new window where keybinds can be changed\nRight Alt to toggle canvas\n Right control to hide top bar");
         }
     }
 }
